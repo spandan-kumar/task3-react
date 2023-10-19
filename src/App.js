c41bacac3941ee233157afe4c5e46af0ba578573
Spandan Kumar <60343774+spandan-kumar@users.noreply.github.com>
2023-10-19 14:07:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './App.css';
+import './ColorPicker.css'; // Add this line
+
+import ColorPicker from './components/ColorPicker';
+
+const App = () => {
+  const colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FFC0CB", "#40E0D0", "#FFA500", "#800080", "#FFB6C1", "#006400"];
+  const [selectedColor, setSelectedColor] = useState('#FF5733');
+
+  const handleColorChange = (color) => {
+    setSelectedColor(color);
+  };
+
+  return (
+    <div className="App">
+      <h1>Color Picker App</h1>
+      <ColorPicker colors={colors} onColorChange={handleColorChange} />
+      <div className="selected-color">
+        Selected Color: <div className="result-box" style={{ backgroundColor: selectedColor }}></div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
